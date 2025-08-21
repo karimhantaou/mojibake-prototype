b959bc2406c91c6911e06eaa22e078aac754642f
karimhantaou <karim.hantaou@gmail.com>
2025-08-21 22:44:31 +0200

    Update gitignore

---
+++ src/Components/BackgroundAnimation.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Sketch from "react-p5";
+export default (props) => {
+
+    let canvasWidth = 500;
+    let canvasHeight = 200;
+
+    let maxPoints = canvasWidth * canvasHeight / 100;
+
+    let totalPoints = 0;
+
+    let maxX = canvasWidth
+    let minX = 0;
+    let maxY = canvasHeight;
+    let minY = 0;
+
+    let pastPositions = [];
+
+    const setup = (p5, canvasParentRef) => {
+        p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
+    };
+
+    const draw = (p5) => {
+        let randomX = p5.random(minX, maxX);
+        let randomY = p5.random(minY, maxY);
+
+        for (let i = 0; i < pastPositions.length; i++) {
+            let pos = pastPositions[i];
+            if (pos.x === randomX && pos.y === randomY) {
+                return; // Si le point est trop proche d'un point existant, on ne le dessine pas
+            }
+            else{
+                p5.circle(p5.random(minX, maxX), p5.random(minY, maxY), 1);
+                totalPoints++;
+                console.log(`Total points: ${totalPoints} / ${maxPoints}`);
+            }
+        }
+        pastPositions.push(p5.createVector(p5.random(minX, maxX), p5.random(minY, maxY)));
+        if(pastPositions.length > maxPoints) {
+            p5.noLoop();
+            console.log("Max points reached, stopping the loop.");
+        }
+    };
+
+    return <Sketch setup={setup} draw={draw} />;
+};
